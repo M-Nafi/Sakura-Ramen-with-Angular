96c5076bf6b4b033b5551e696d8ececb05fa43ee
M-Nafi <nafi-m@gmx.de>
2024-08-18 11:48:08 +0200

    created first component with hello world

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [],
+    template: '<h1>Hallo Welt</h1>',
+    styles: ['h1 {color: red;}']
+})
+export class LandingPageComponent {}
